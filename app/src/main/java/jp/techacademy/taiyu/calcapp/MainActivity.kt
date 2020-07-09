0bf0e269efb1d737bfdc8fe0ee625147b7164614
taiyu0526 <taiyukim@gmail.com>
2020-07-09 22:09:58 +0900

    計算アプリ（２）

--- app/src/main/java/jp/techacademy/taiyu/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taiyu/calcapp/MainActivity.kt
@@ -5,29 +5,57 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.view.View
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity(),View.OnClickListener{
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        additionbutton.setOnClickListener(this)
-
-        button1.setOnClickListener{
-            textView.text = editText.text.toString()
-            textView2.text = editText2.text.toString()
-        }
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
 
     }
 
-        override fun onClick(v: View?) {
-            val intent = Intent(this,
-                    SecondActivity::class.java)
-            intent.putExtra("keisan1", textView.text.toString())
-            intent.putExtra("keisan2", textView2.text.toString())
-            startActivity(intent)
+
+    override fun onClick(v: View) {
+
+        val intent = Intent(this, SecondActivity::class.java)
+
+        var str1: String = editText.text.toString()//エディットテキストの値をstring型
+        var str2: String = editText2.text.toString()
+
+        var num1: Double = str1.toDouble()//string型をdouble型へ
+        var num2: Double = str2.toDouble()
+
+        var result = 0.0
+
+        if (str1.length == 0 || str2.length == 0) {
+            
+
+
+        } else {
+
+
+            if (v.id == R.id.button1) {
+                result = num1 + num2
+            } else if (v.id == R.id.button2) {
+                result = num1 - num2
+            } else if (v.id == R.id.button3) {
+                result = num1 * num2
+            } else if (v.id == R.id.button4) {
+                result = num1 / num2
+            }
         }
 
-
+        intent.putExtra("result", result)
+        startActivity(intent)
     }
+}
+
+
+
 
