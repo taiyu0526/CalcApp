54476b962cf7152ca4579bda8dbd040b49a14d0b
taiyu0526 <taiyukim@gmail.com>
2020-07-10 22:26:03 +0900

    四則計算アプリ（再提出）

--- app/src/main/java/jp/techacademy/taiyu/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/taiyu/calcapp/SecondActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_result.*
-import kotlinx.android.synthetic.main.activity_main.textView as textView1
+
 
 class SecondActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -13,9 +13,8 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val keisan1 =intent.getFloatExtra("keisan1", 0f)
-        val keisan2 =intent.getFloatExtra("keisan2", 0f)
-        textView.text = "${  keisan1 + keisan2  }"
+       var result = intent.getDoubleExtra("result", 0.0)
+        textView.text = "${result}"
 
     }
 }
